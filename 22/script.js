442c3794c481e5a4fb388d1756a3c8b4c11030e5
iampopov <ivantsar@gmail.com>
2020-02-13 12:38:36 -0700

    Solved: Write a function that takes in a string and returns the string with all lowercased letters uppercase, and all uppercased letters lowercase

--- 22/script.js
+++ 22/script.js
@@ -11,12 +11,25 @@
 
 
 function changeCase(arr) {
-    return arr;
+    //return arr.length
+    reversed = []
+    for (let i = 0; i < arr.length; i++) {
+        //console.log(arr[i]);
+        if (arr[i] === arr[i].toUpperCase()) {
+            //console.log(arr[i] + " is upper");
+            reversed.push(arr[i].toLowerCase())
+        } else {
+            //console.log(arr[i] + ' is lower');
+            reversed.push(arr[i].toUpperCase())
+        }
+        
+    }
+    return reversed.toString().split(',').join('');
 }
 
 console.log(changeCase("hello world"));
 console.log(changeCase("HELLO WORLD"));
 console.log(changeCase("1a2b3c4d5e"));
-console.log(changeCase("OoOoOhHh! MaGiCaL!!!"));
+ console.log(changeCase("OoOoOhHh! MaGiCaL!!!"));
 
 
