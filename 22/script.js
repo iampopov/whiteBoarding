dfd85a32365a3ac9b4da7d1ec669c05822089740
iampopov <ivantsar@gmail.com>
2020-02-13 11:29:39 -0700

    set up coding challenge

---
+++ 22/script.js
@@ -0,0 +1,22 @@
+// Write a function that takes in a string and returns the string with all lowercased letters uppercase, and all uppercased letters lowercase
+// Examples:
+// Input: "hello world"
+// Output: "HELLO WORLD"
+// Input: "HELLO WORLD"
+// Output: "hello world"
+// Input: "1a2b3c4d5e"
+// Output: "1A2B3C4D5E"
+// Input: "OoOoOhHh! MaGiCaL!!!"
+// Output: "oOoOoHhH! mAgIcAl!!!"
+
+
+function changeCase(arr) {
+    return arr;
+}
+
+console.log(changeCase("hello world"));
+console.log(changeCase("HELLO WORLD"));
+console.log(changeCase("1a2b3c4d5e"));
+console.log(changeCase("OoOoOhHh! MaGiCaL!!!"));
+
+
