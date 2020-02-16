3d6d201a83f88b2444907070ae5a29b09d1d598b
iampopov <ivantsar@gmail.com>
2020-02-15 21:02:13 -0700

    added find unique challange

--- kata/findUnique/script.js
+++ kata/findUnique/script.js
@@ -1,5 +1,6 @@
 function findUniq(arr) {
-    return arr
+    //return arr.filter((item, i, ar) => ar.indexOf(item) === i) // this works great to find distinct numbers but not exactly what I am after!
+    
   }
 
   console.log(findUniq([0, 1, 0]));
