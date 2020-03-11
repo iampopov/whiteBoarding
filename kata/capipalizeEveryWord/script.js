81cbda55beacd5ab593464de21a49e70b0a2da3a
iampopov <ivantsar@gmail.com>
2020-03-11 15:01:10 -0600

    added capitalize every word first letter solution

---
+++ kata/capipalizeEveryWord/script.js
@@ -0,0 +1,9 @@
+//capitalizes every word first letter
+
+function toJadenCase( str ){
+    return str.toLowerCase().split(' ').map(s => s.charAt(0).toUpperCase() + s.substring(1)).join(' ')
+} 
+
+  console.log(toJadenCase("How can mirrors be real if our eyes aren't real"));
+  
+  
