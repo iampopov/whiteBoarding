95b3c3559e14a7f0b186ce3d2370bdf2b7bf09bb
iampopov <ivantsar@gmail.com>
2020-03-15 21:10:27 -0600

    added coupon verifier problem solution

--- kata/couponValidator/script.js
+++ kata/couponValidator/script.js
@@ -8,3 +8,4 @@ function checkCoupon(enteredCode, correctCode, currentDate, expirationDate){
   console.log(checkCoupon("123", "123", "July 9, 2015", "July 2, 2015"));//false
   console.log(checkCoupon('123','123','September 5, 2014','October 1, 2014'));//true
   console.log(checkCoupon('123a','123','September 5, 2014','October 1, 2014'));//false
+//solved
