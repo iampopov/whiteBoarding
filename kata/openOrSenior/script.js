996b6952a957f586dfc2a57706f9fe2157dc01f3
IamPopov <ivantsar@gmail.com>
2020-03-28 09:42:07 -0600

    solved openOrSenior problem

--- kata/openOrSenior/script.js
+++ kata/openOrSenior/script.js
@@ -17,8 +17,19 @@
 
 
 function openOrSenior(data){
-    return data
-  }
+    
+    const result = []
+
+    for (let i = 0; i < data.length; i++) {
+        
+        if (data[i][0] > 54 && data[i][1] > 6) {
+            result.push('Senior')
+        } else {
+            result.push('Open')
+        }
+    }
+    return result;
+}
 
   console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]])); //['Open', 'Senior', 'Open', 'Senior']
   console.log(openOrSenior([[3, 12],[55,1],[91, -2],[54, 23]])); //['Open', 'Open', 'Open', 'Open']
