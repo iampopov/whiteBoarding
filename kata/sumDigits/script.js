39c3f6d8d3dccc1271490012b9b3336b8de6583d
IamPopov <ivantsar@gmail.com>
2020-04-01 23:13:10 -0600

    added and solved sum digits

--- kata/sumDigits/script.js
+++ kata/sumDigits/script.js
@@ -2,11 +2,16 @@ const sumDigits = (number) => {
 
     const toNum = n => parseInt(n);
 
-    return (""+number).replace(/[^0-9]/g, '').split("").reduce((a, b) => toNum(a) + toNum(b));
-    
+    let result = (""+number).replace(/[^0-9]/g, '').split("").reduce((a, b) => toNum(a) + toNum(b));
+
+    return parseInt(result)
+
+  
 }
 
   console.log(sumDigits(10)); //1
   console.log(sumDigits(99)); //18
   console.log(sumDigits(-32)); //5
+  console.log(sumDigits(000)); //0
+  console.log(sumDigits(11)); //2
   
