6dbbb8f0d1c1993cbfc15d0ed135ef8bfeafdd60
IamPopov <ivantsar@gmail.com>
2020-04-04 19:01:32 -0600

    added predict age problem

---
+++ kata/predictAge/script.js
@@ -0,0 +1,8 @@
+function predictAge( age1, age2, age3, age4, age5, age6, age7, age8 ){
+    
+    return age1;
+} 
+
+  console.log(predictAge(65,60,75,55,60,63,64,45)); //86
+  //console.log(predictAge(false));
+  
