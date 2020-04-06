1e35ae3ae07a82f6c975d9399e6651ea8f31c280
IamPopov <ivantsar@gmail.com>
2020-04-05 22:11:55 -0600

    added longest num problem

---
+++ kata/findLongest/script.js
@@ -0,0 +1,9 @@
+function findLongest( arr ){
+     
+    return arr;
+} 
+
+  console.log(findLongest([1, 10, 100]));
+  console.log(findLongest([9000, 8, 800]));
+  console.log(findLongest([8, 900, 500]));
+  
