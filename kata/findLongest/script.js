b2ff92ebccb52c041f3405b9d2599d522843be7e
IamPopov <ivantsar@gmail.com>
2020-04-05 22:22:15 -0600

    added solution to longest num problem

--- kata/findLongest/script.js
+++ kata/findLongest/script.js
@@ -1,9 +1,13 @@
 function findLongest( arr ){
-     
-    return arr;
+  
+    const toStr = arr.map(val => String(val));
+    const longs = toStr.reduce((a,b) => {
+        return b.length > a.length ? b : a;
+    })
+  return Number(longs)
 } 
 
-  console.log(findLongest([1, 10, 100]));
-  console.log(findLongest([9000, 8, 800]));
-  console.log(findLongest([8, 900, 500]));
+  console.log(findLongest([1, 10, 100])); //100
+  console.log(findLongest([9000, 8, 800])); //9000
+  console.log(findLongest([8, 900, 500])); //900
   
