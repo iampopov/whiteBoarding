894946cc4685e744f3494704c8211e04ab39301d
IamPopov <ivantsar@gmail.com>
2020-04-06 21:58:17 -0600

    added isAnagram problem

---
+++ kata/isAnagram/script.js
@@ -0,0 +1,12 @@
+function isAnagram( test, original ){
+    
+    return test;
+} 
+
+  console.log(isAnagram("foefet", "toffee")); //true
+  console.log(isAnagram("Buckethead", "DeathCubeK")); //true
+  console.log(isAnagram("Twoo", "WooT")); //true
+  console.log(isAnagram("dumble", "bumble")); //false
+  console.log(isAnagram("ound", "round")); //false
+  console.log(isAnagram("apple", "pale")); //false
+  
