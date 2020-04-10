f39eba7265fba1a80057c1074fe7ba8b98c9ade9
IamPopov <ivantsar@gmail.com>
2020-04-09 22:46:53 -0600

    added removeUrlAnchor problem solution

--- kata/removeUrlAnchor/script.js
+++ kata/removeUrlAnchor/script.js
@@ -1,11 +1,11 @@
 function removeUrlAnchor( url ){
     
-    if (url.substring(0, url.indexOf('#'))) {
-       return url.substring(0, url.indexOf('#')) 
-    } else {
-        return url;
-    }
-    
+    // if (url.substring(0, url.indexOf('#'))) {
+    //    return url.substring(0, url.indexOf('#')) 
+    // } else {
+    //     return url;
+    // }
+    return url.substring(0, url.indexOf('#')) ? url.substring(0, url.indexOf('#')) : url;
 } 
 
   console.log(removeUrlAnchor('www.codewars.com#about'));
