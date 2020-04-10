dceafd11a9a32928d9e8673c2cb497afd04f4dab
IamPopov <ivantsar@gmail.com>
2020-04-09 22:12:20 -0600

    added removeUrlAnchor problem

---
+++ kata/removeUrlAnchor/script.js
@@ -0,0 +1,13 @@
+function boolToWord( bool ){
+    //...
+    // if (bool) { 
+    //     bool = "Yes";
+    //     return bool}
+    //     bool = "No";
+    //     return bool  
+    return (bool ? "Yes" : "No");
+} 
+
+  console.log(boolToWord(true));
+  console.log(boolToWord(false));
+  
