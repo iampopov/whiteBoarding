2b5a993b4f108edb05659320d3e12cf13272ae8a
Ivan Popov <ivantsar@gmail.com>
2020-04-14 20:14:57 -0600

    added line number problem a better solution

--- kata/lineNumbering/script.js
+++ kata/lineNumbering/script.js
@@ -1,15 +1,19 @@
 var number=function(array){
 
-const result = [];
+// const result = [];
 
-if ( array === [] ) {
-  return array;
-} else {
-  for (let index = 0; index < array.length; index++) {
-    result.push(`${index+1}: ${array[index]}`)    
-  }
-}
-return result;
+// if ( array === [] ) {
+//   return array;
+// } else {
+//   for (let index = 0; index < array.length; index++) {
+//     result.push(`${index+1}: ${array[index]}`)    
+//   }
+// }
+// return result;
+
+return array.map(function (line, index) {
+  return (index + 1) + ": " + line;
+});
 
 }
 
