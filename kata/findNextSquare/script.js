228c25e7008040fb7db3a17df4beffc8eedabcba
Ivan Popov <ivantsar@gmail.com>
2020-04-15 22:14:17 -0600

    added perfect square problem solution

--- kata/findNextSquare/script.js
+++ kata/findNextSquare/script.js
@@ -1,10 +1,10 @@
 function findNextSquare( sq ){
-    if (Number.isInteger(Math.sqrt(sq))) {
-      return Math.pow((Math.sqrt(sq) + 1), 2)
-    } else {
-      return -1
-    }
-
+    // if (Number.isInteger(Math.sqrt(sq))) {
+    //   return Math.pow((Math.sqrt(sq) + 1), 2)
+    // } else {
+    //   return -1
+    // }
+    return Math.sqrt(sq)%1? -1 : Math.pow(Math.sqrt(sq)+1,2);
 } 
 
   console.log(findNextSquare(121));
