31e2924fc13ada954f6f93184cce870671093a72
Ivan Popov <ivantsar@gmail.com>
2020-05-20 16:20:03 -0600

    added sum of add row problem

--- kata/sumOfOdd/script.js
+++ kata/sumOfOdd/script.js
@@ -1,10 +1,21 @@
+/*
+Given the triangle of consecutive odd numbers:
+            1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+rowSumOddNumbers(1); // 1
+rowSumOddNumbers(2); // 3 + 5 = 8
+*/
 const rowSumOddNumbers = (number) => {
-  
+
      
     return number;
 
 }
 
   console.log(rowSumOddNumbers(1)); //1
+  console.log(rowSumOddNumbers(2)); //8
   console.log(rowSumOddNumbers(42)); //74088
   
