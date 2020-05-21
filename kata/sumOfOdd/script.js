1b5bceb8efe588074be11cba17d7d3749c4a5c23
Ivan Popov <ivantsar@gmail.com>
2020-05-20 19:17:09 -0600

    solved sum of odd row problem

--- kata/sumOfOdd/script.js
+++ kata/sumOfOdd/script.js
@@ -9,13 +9,24 @@ rowSumOddNumbers(1); // 1
 rowSumOddNumbers(2); // 3 + 5 = 8
 */
 const rowSumOddNumbers = (number) => {
+  //var nn = number.toString().split()
+ // console.log(nn, number);
+  nthOdd = (number - 1) * number + 1 //(2 * number - 1)
+  //console.log(nthOdd);
+  let odd = []
+  for (let i = 0; i < number; i++) {
+    odd.push(nthOdd)
+    //console.log(nthOdd);
+    nthOdd = nthOdd + 2
+        
+  }
 
-     
-    return number;
+    return odd.reduce((a,b) => a+b);
 
 }
 
   console.log(rowSumOddNumbers(1)); //1
   console.log(rowSumOddNumbers(2)); //8
+  console.log(rowSumOddNumbers(3)); //27
   console.log(rowSumOddNumbers(42)); //74088
   
